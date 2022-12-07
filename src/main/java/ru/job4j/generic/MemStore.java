c70e006b58008db23a9a42f8b62dfe5af2664f38
marat92 <sodnopov@list.ru>
2022-12-07 20:33:02 +0700

    5.2.2. Реализовать Store<T extends Base> [#157 #368421]

--- src/main/java/ru/job4j/generic/MemStore.java
+++ src/main/java/ru/job4j/generic/MemStore.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class MemStore<T extends Base> implements Store<T> {
-
     private final Map<String, T> storage = new HashMap<>();
 
     @Override
@@ -14,27 +13,28 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        if (storage.containsKey(id)) {
-            storage.replace(id, model);
-            return true;
-        }
-        return false;
+         return storage.keySet().stream()
+                 .filter(e -> e.equals(id))
+                 .findFirst()
+                 .map((e) -> storage.replace(e, model))
+                 .isPresent();
     }
 
     @Override
     public boolean delete(String id) {
-        if (storage.containsKey(id)) {
-            storage.remove(id, findById(id));
-            return true;
-        }
-        return false;
+        return storage.keySet().stream()
+                .filter(e -> e.equals(id))
+                .findFirst()
+                .map(e -> storage.remove(id, storage.get(id)))
+                .isPresent();
     }
 
     @Override
     public T findById(String id) {
-        if (storage.containsKey(id)) {
-            return storage.get(id);
-        }
-        return null;
+        return storage.keySet().stream()
+                .filter(e -> e.equals(id))
+                .findFirst()
+                .map(storage::get)
+                .orElse(null);
     }
 }
