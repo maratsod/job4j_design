47f66dbe5f2797af1e5d2097c2e3d39e88e1af09
marat92 <sodnopov@list.ru>
2022-12-09 14:55:30 +0700

    1. Динамический список на массиве. [#158 #368951]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import java.util.*;
 
 public class SimpleArrayList<T> implements SimpleList<T> {
+
     private T[] container;
     private int size;
     private int modCount;
