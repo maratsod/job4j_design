531c5bd9948bfe07545f382a26ed27c7c50c5086
marat92 <sodnopov@list.ru>
2022-12-09 14:53:49 +0700

    1. Динамический список на массиве. [#158 #368951]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -6,40 +6,46 @@ public class SimpleArrayList<T> implements SimpleList<T> {
     private T[] container;
     private int size;
     private int modCount;
+    private int point = 0;
 
     public SimpleArrayList(int capacity) {
         this.container = (T[]) new Object[capacity];
     }
 
+    private void grow() {
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+
     @Override
     public void add(T value) {
         if (container.length == size) {
-            container = Arrays.copyOf(container, container.length * 2);
+            grow();
         }
-            container[size++] = value;
-            modCount++;
+        container[size++] = value;
+        modCount++;
     }
 
     @Override
     public T set(int index, T newValue) {
+        T t = container[index];
         container[index] = newValue;
-        size++;
-        modCount++;
-        return container[index];
+        return t;
     }
 
     @Override
     public T remove(int index) {
-        modCount++;
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
+        T t = container[index];
         System.arraycopy(container, index + 1, container, index, container.length - index - 1);
         container[container.length - 1] = null;
-        return container[index];
+        modCount++;
+        size--;
+        return t;
     }
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
@@ -52,23 +58,21 @@ public class SimpleArrayList<T> implements SimpleList<T> {
     public Iterator<T> iterator() {
         int expectedModCount = modCount;
         return new Iterator<T>() {
-
             @Override
             public boolean hasNext() {
-                return size < container.length;
+                return point < size;
             }
 
             @Override
             public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                if (!(size >= container.length)) {
-                    throw new NoSuchElementException();
-                }
-                return container[size++];
+                return container[point++];
             }
-
         };
     }
 }
