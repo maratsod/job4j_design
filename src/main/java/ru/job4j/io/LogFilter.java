28a2a5a2bc1cd5edf7dbc06ccffc8c2e4755028c
marat92 <sodnopov@list.ru>
2023-01-03 20:32:30 +0700

    0.4. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class LogFilter {
-    public List<String> filter(String file) {
+    public static List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             list = in.lines()
@@ -18,22 +18,17 @@ public class LogFilter {
     }
 
     public static void save(List<String> log, String file) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(file)
-                ))) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(file))) {
             for (String line : log) {
                 out.println(line);
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter();
-        List<String> log = logFilter.filter("log.txt");
+        List<String> log = filter("log.txt");
         save(log, "404.txt");
         log.forEach(System.out::println);
     }
