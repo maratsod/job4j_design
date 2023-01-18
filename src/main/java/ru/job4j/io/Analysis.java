63ee830f8ce79907501cbf356f5dd01b7998cd56
marat92 <sodnopov@list.ru>
2023-01-18 15:31:02 +0700

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        ArrayList<String> rsl = new ArrayList<>() ;
+        ArrayList<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
             in.lines().forEach(rsl::add);
@@ -21,7 +21,7 @@ public class Analysis {
                     writer.printf(";");
                     check = true;
                 } else if (check) {
-                    array = str.split(" " , 2);
+                    array = str.split(" ", 2);
                     writer.printf("%s", array[1]);
                     writer.printf("%s%n", ";");
                     check = false;
